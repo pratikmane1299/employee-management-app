2a37b6cf25f1c47d8ec4a70ac82e485292a3b4af
pratik mane <pratik.mane1299@gmail.com>
2021-01-25 20:36:35 +0530

    :sparkles: setup knex and objection and connect to postgres

---
+++ server/knexfile.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    },
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds',
+    },
+  },
+};
